a4ff064bc7823accbdd6f61d00c82db7c6a7b7a0
Dylan Raiff <d.raiff@icloud.com>
2022-09-12 22:29:47 -0400

    Add checking of command permissions and passing of arguments.

--- event/guild/interactionCreate.js
+++ event/guild/interactionCreate.js
@@ -1,4 +1,5 @@
-const { InteractionType } = require("discord.js");
+const { InteractionType, ApplicationCommandOptionType } = require("discord.js");
+const { options } = require("../../commands/utils/createconfigmenu");
 
 module.exports = (interaction, client) => {
     if (interaction.type !== InteractionType.ApplicationCommand) return;
@@ -6,8 +7,24 @@ module.exports = (interaction, client) => {
     const cmd = client.slash.get(interaction.commandName);
     console.assert(cmd, `Command ${interaction.commandName} not found!`);
 
+    if (cmd.permissions) {
+        if (!client.guilds.cache.get(interaction.guild.id).members.cache
+            .get(interaction.member.id).permissions.has(cmd.permissions || [])) {
+            return interaction.reply(`You need the \`${cmd.permissions}\` permission to use this command!`)
+        }
+    }
+
+    let args = {};
+    interaction.options.data.forEach(element => {
+        if (element.type == ApplicationCommandOptionType.Subcommand) {
+            args.__subcommand = element.name;
+            element.options.forEach(subElement =>  args[subElement.name] = subElement.value);
+        }
+        else args[element.name] = element.value;
+    });
+
     try {
-        cmd.run(client, interaction);
+        cmd.run(client, interaction, args);
     } catch (e) {
         interaction.reply({ content: `An error occured:\n${e.message}` })
     }
