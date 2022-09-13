0c3e925be74f84113f45b2690e734a909c78b3ff
Dylan Raiff <d.raiff@icloud.com>
2022-09-12 22:28:10 -0400

    Implement loading event listeners.

--- loader/load_events.js
+++ loader/load_events.js
@@ -1,5 +1,6 @@
 const client_event = (type) => require(`../event/client/${type}`)
 const guild_event = (type) => require(`../event/guild/${type}`)
+const menu_event = (type) => require(`../event/menu/${type}`)
 
 module.exports = (client) => {
     // Client Events
@@ -7,4 +8,9 @@ module.exports = (client) => {
 
     // Guild Events
     client.on('interactionCreate', (param) => guild_event('interactionCreate')(param, client));
+    client.on('guildMemberAdd', (param => guild_event('guildMemberAdd')(param, client)));
+
+    // Menu Events
+    client.on('interactionCreate', (param) => menu_event('selectMenuUpdate')(param, client));
+
 };
